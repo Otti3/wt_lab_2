73bfd9ce9f278fcae3caaecdd01747a9588f017a
Ottis <vip.mozol@bk.ru>
2022-05-27 15:00:57 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import {Button, Container, IconButton, TextField} from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container className="app" maxWidth="xl" disableGutters = {true}>
+              <Container className="header" disableGutters={true}>
+                  <IconButton size="small">
+                        
+                  </IconButton>
+              </Container>
+              <TextField fullWidth>
+              </TextField>
+      </Container>
+
   );
 }
 
